7c08bcd1d598385552266674f5cb23b6540b4eb6
mizu-bai <shiragawa4519@outlook.com>
2024-01-26 18:06:05 +0800

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # venus2gro
 
-Extract coordinates and momenta from VENUS96 output and write trajectories in GROMACS gro format.
+Extract coordinates and momenta from VENUS96 output and write trajectories in GROMACS gro and GROMOS g96 format.
 
 Author: mizu-bai
 
@@ -10,6 +10,8 @@ Author: mizu-bai
 - numpy
 
 ## Usage
+
+### `venus2gro`
 
 ```bash
 $ python3 venus2gro.py -h
@@ -24,6 +26,24 @@ optional arguments:
   -g GRO, --gro GRO     Template gro file (optional)
   -o OUTPUT, --output OUTPUT
                         Output gro file (optional, defaults to traj_idx.gro)
+  -r REORDER, --reorder REORDER
+                        Reorder map (optional)
+  --no-split            If specified, write all trajectories in one file. (optional)
+```
+
+### `venus2g96`
+
+```bash
+usage: venus2g96.py [-h] -v VENUS [-o OUTPUT] [-r REORDER] [--no-split]
+
+Convert trajectory in VENUS96 output to g96 format with coordinates and velocities included.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v VENUS, --venus VENUS
+                        VENUS96 output file
+  -o OUTPUT, --output OUTPUT
+                        Output g96 file (optional, defaults to traj_idx.g96)
   -r REORDER, --reorder REORDER
                         Reorder map (optional)
   --no-split            If specified, write all trajectories in one file. (optional)
@@ -44,8 +64,18 @@ Run this command, then 5 trajectories in gro format will be created.
 $ python3 ../src/venus2gro.py -v ch4.out -g template.gro -o ch4_traj.gro -r reorder.txt
 ```
 
+To convert to `g96` format.
+
+```bash
+$ python3 ../src/venus2g96.py -v ch4.out -o ch4_traj.gro -r reorder.txt
+```
+
+Then the `gro` or `g96` trajectories can be converted to `xtc` or `trr` format with the help of GROMACS so that analysis can be easier.
+
 ## Reference
 
 (1) Hase, W. L.; Duchovic, R. J.; Hu, X.; Komornicki, A.; Lim, K. F.; Lu, D.-H.; Peslherbe, G. H.; Swamy, K. N.; Vande Linde, S. R.; Varandas, A., Wang, H.; Wolf, R. J. VENUS96: A general chemical dynamics computer program, _Quantum Chemical Program Exchange (QCPE) Bulletin_, **1996**, _16_ (4), 671. https://www.depts.ttu.edu/chemistry/Venus/index.php
 
 (2) Abraham, M. J.; Murtola, T.; Schulz, R.; Páll, S.; Smith, J. C.; Hess, B.; Lindahl, E. GROMACS: High Performance Molecular Simulations through Multi-Level Parallelism from Laptops to Supercomputers. _SoftwareX_ **2015**, _1–2_, 19–25. https://doi.org/10.1016/j.softx.2015.06.001.
+
+(3) Scott, W. R. P.; Hünenberger, P. H.; Tironi, I. G.; Mark, A. E.; Billeter, S. R.; Fennen, J.; Torda, A. E.; Huber, T.; Krüger, P.; van Gunsteren, W. F. The GROMOS Biomolecular Simulation Program Package. _J. Phys. Chem. A_ **1999**, _103_ (19), 3596–3607. https://doi.org/10.1021/jp984217f.
--- src/venus2g96.py
+++ src/venus2g96.py
@@ -3,55 +3,62 @@ import glob
 import os
 from copy import deepcopy
 from dataclasses import dataclass
-from typing import List, Union
+from typing import List, Tuple, Union
 
 import numpy as np
 
 
 @dataclass
-class GroMol:
+class G96Mol:
     title: str
-    num_atoms: int
-    resi_num: Union[List[int], np.ndarray]
-    resi_name: List[str]
-    atom_name: List[str]
-    atom_num: Union[List[int], np.ndarray]
-    xyz: np.ndarray  # nm
-    vel: np.ndarray  # nm/ps
-    box_size: np.ndarray = np.zeros(3)
+    timestep: Tuple[int, float]
+    position: np.array
+    velocity: np.array
+    box: np.array = np.zeros(3)
 
     def __str__(self) -> str:
-        gro_contents = []
-        gro_contents.append(f"{self.title}")
-        gro_contents.append(f"{self.num_atoms}")
+        g96_contents = []
 
-        for i in range(self.num_atoms):
-            gro_contents.append(
-                f"{self.resi_num[i]:5d}"
-                f"{self.resi_name[i]:5s}"
-                f"{self.atom_name[i]:5s}"
-                f"{self.atom_num[i]:5d}"
-                f"{self.xyz[i][0]:8.3f}"
-                f"{self.xyz[i][1]:8.3f}"
-                f"{self.xyz[i][2]:8.3f}"
-                f"{self.vel[i][0]:8.4f}"
-                f"{self.vel[i][1]:8.4f}"
-                f"{self.vel[i][2]:8.4f}"
-            )
+        # title
+        g96_contents.append("TITLE")
+        g96_contents.append(self.title)
+        g96_contents.append("END")
 
-        gro_contents.append(
-            f"{self.box_size[0]:10.6f}"
-            f"{self.box_size[1]:10.6f}"
-            f"{self.box_size[2]:10.6f}"
+        # timestep
+        g96_contents.append("TIMESTEP")
+        g96_contents.append(f"{self.timestep[0]:15d}{self.timestep[1]:15.6f}")
+        g96_contents.append("END")
+
+        # position
+        g96_contents.append("POSITIONRED")
+
+        for pos in self.position:
+            g96_contents.append(f"{pos[0]:15.9f}{pos[1]:15.9f}{pos[2]:15.9f}")
+
+        g96_contents.append("END")
+
+        # velocity
+        g96_contents.append("VELOCITYRED")
+
+        for vel in self.velocity:
+            g96_contents.append(f"{vel[0]:15.9f}{vel[1]:15.9f}{vel[2]:15.9f}")
+
+        g96_contents.append("END")
+
+        # box
+        g96_contents.append("BOX")
+        g96_contents.append(
+            f"{self.box[0]:15.9f}{self.box[1]:15.9f}{self.box[2]:15.9f}"
         )
+        g96_contents.append("END")
 
-        return "\n".join(gro_contents)
+        return "\n".join(g96_contents)
 
 
 def _parse_args():
     # prepare argument parser
     parser = argparse.ArgumentParser(
-        description="Convert trajectory in VENUS96 output to gro format "
+        description="Convert trajectory in VENUS96 output to g96 format "
         "with coordinates and velocities included."
     )
 
@@ -64,20 +71,11 @@ def _parse_args():
     )
 
     parser.add_argument(
-        "-g",
-        "--gro",
-        type=str,
-        help="Template gro file (optional)",
-        default="template.gro",
-        required=True,
-    )
-
-    parser.add_argument(
         "-o",
         "--output",
         type=str,
-        help="Output gro file (optional, defaults to traj_idx.gro)",
-        default="traj.gro",
+        help="Output g96 file (optional, defaults to traj_idx.g96)",
+        default="traj.g96"
     )
 
     parser.add_argument(
@@ -106,11 +104,6 @@ def _check_input_files(args):
             f"VENUS96 output file {args.venus} does not exist!"
         )
 
-    if not os.path.exists(args.gro):
-        raise FileNotFoundError(
-            f"Template gro file {args.gro} does not exist!"
-        )
-
     if args.reorder:
         reorder_map = args.reorder
         if not os.path.exists(reorder_map):
@@ -119,27 +112,23 @@ def _check_input_files(args):
             )
 
 
-def venus2gro(
+def venus2g96(
     venus_out: str,
-    template_gro: str,
-    out_gro: str,
+    out_g96: str,
     reorder: Union[str, None],
     split: bool = True,
 ) -> List[str]:
-    """Convert VENUS96 output file to gro file.
+    """Convert VENUS96 output file to g96 file.
 
     Args:
         venus_out (str): Path to VENUS96 output file.
-        template_gro (str): Path to template gro file.
-        out_gro (str): Path to output gro file(s).
+        out_g96 (str): Path to output g96 file(s).
         reorder (str): Path to reorder map text file.
-            Defaults to None, i.e. atoms in VENUS96 output file shares the same
-            order in template gro file.
         split (bool): Whether to split different trajectories.
             Defaults to True.
 
     Returns:
-        traj_gro_list (List[str]): List of paths to all trajectories extracted
+        traj_g96_list (List[str]): List of paths to all trajectories extracted
             from VENUS96 output file.
     """
 
@@ -147,27 +136,7 @@ def venus2gro(
     if reorder is not None:
         reorder_map = np.loadtxt(reorder, dtype=np.int32) - 1
 
-    """Section 1. Parse template gro"""
-    temp_contents = open(template_gro, "r").readlines()
-
-    temp_title = temp_contents.pop(0).rstrip()
-    temp_num_atoms = int(temp_contents.pop(0))
-
-    temp_resi_num = []
-    temp_resi_name = []
-    temp_atom_name = []
-    temp_atom_num = []
-
-    for _ in range(temp_num_atoms):
-        line = temp_contents.pop(0)
-        temp_resi_num.append(int(line[0:5]))
-        temp_resi_name.append(line[5:10].rstrip())
-        temp_atom_name.append(line[10:15].rstrip())
-        temp_atom_num.append(int(line[15:20]))
-
-    temp_box_size = [float(x) for x in temp_contents.pop(0).split()]
-
-    """Section 2. Parse VENUS96 output file"""
+    """Section 1. Parse VENUS96 output file"""
     contents = open(venus_out, "r").readlines()
     num_lines = len(contents)
     cur_line_idx = 0
@@ -188,9 +157,6 @@ def venus2gro(
         # parse num_atoms
         if anchor_num_atoms in line:
             num_atoms = int(line.split("=")[-1])
-            assert num_atoms == temp_num_atoms, \
-                "Number of atoms in VENUS96 output and template gro file" \
-                " mismatch, please check these files."
 
         # parse masses
         if anchor_masses in line:
@@ -242,56 +208,50 @@ def venus2gro(
                     xyz[old_idx] = _xyz[new_idx]
                     vel[old_idx] = _vel[new_idx]
 
-            # create GroMol
-            title = f"{temp_title} Traj {cur_traj_idx}, t= {cur_time:.4f} ps"
-            gro_mol = GroMol(
-                title=title,
-                num_atoms=num_atoms,
-                resi_num=temp_resi_num,
-                resi_name=temp_resi_name,
-                atom_name=temp_atom_name,
-                atom_num=temp_atom_num,
-                xyz=np.array(xyz),
-                vel=np.array(vel),
-                box_size=np.array(temp_box_size),
+            # create G96Mol
+            g96_mol = G96Mol(
+                title="",
+                timestep=[cur_cycle, cur_time],
+                position=xyz,
+                velocity=vel,
             )
 
-            traj_list[-1].append(gro_mol)
+            traj_list[-1].append(g96_mol)
 
         cur_line_idx += 1
 
-    """Section 3. Write trajectories to gro file(s)"""
+    """Section 2. Write trajectories to g96 file(s)"""
     def _auto_backup_file(file):
         if not os.path.exists(file):
             return
 
         file_dir = os.path.dirname(file)
         file_name = os.path.basename(file)
-        old_file_pattern = f"#{file_name.replace('.gro', '')}.*.gro#"
+        old_file_pattern = f"#{file_name.replace('.g96', '')}.*.g96#"
         old_files = sorted(glob.glob(os.path.join(
             file_dir,
             old_file_pattern,
         )))
         num_old_files = len(old_files)
         new_backup_file_name = \
-            f"#{file_name.replace('.gro', '')}.{num_old_files + 1}.gro#"
+            f"#{file_name.replace('.g96', '')}.{num_old_files + 1}.g96#"
         os.rename(file, new_backup_file_name)
 
-    traj_gro_list = []
+    traj_g96_list = []
 
     if not split:
         traj_list = np.concatenate(traj_list).reshape(1, -1).tolist()
 
     for (traj_idx, traj) in enumerate(traj_list):
-        traj_gro_name = out_gro.replace(".gro", f"_{traj_idx + 1}.gro")
-        _auto_backup_file(traj_gro_name)
-        with open(traj_gro_name, "w") as f:
+        traj_g96_name = out_g96.replace(".g96", f"_{traj_idx + 1}.g96")
+        _auto_backup_file(traj_g96_name)
+        with open(traj_g96_name, "w") as f:
             for frame in traj:
                 f.writelines(f"{str(frame)}\n")
 
-        traj_gro_list.append(traj_gro_name)
+            traj_g96_list.append(traj_g96_name)
 
-    return traj_gro_list
+    return traj_g96_list
 
 
 if __name__ == "__main__":
@@ -302,10 +262,9 @@ if __name__ == "__main__":
     _check_input_files(args)
 
     # parse output
-    traj_list = venus2gro(
+    traj_list = venus2g96(
         venus_out=args.venus,
-        template_gro=args.gro,
-        out_gro=args.output,
+        out_g96=args.output,
         reorder=args.reorder,
         split=(not args.no_split),
     )
